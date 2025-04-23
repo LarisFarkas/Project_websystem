4bd579ea02d99e212063a3629983b30047c85223
Moha <mohammad.awad0110@gmail.com>
2025-04-23 12:12:58 +0200

    new update

---
+++ pages/dashboard_content.php
@@ -0,0 +1,9 @@
+<div class="container-fluid py-5">
+  <div class="row">
+    <div class="col">
+      <h1 class="display-4 fw-bold text-primary">Dashboard</h1>
+      <hr class="border-light">
+    </div>
+  </div>
+
+</div>
---
+++ pages/index_content.php
@@ -0,0 +1,5 @@
+<div class="container mt-5">
+  <h1 class="mb-4">Welcome to the Dashboard</h1>
+  <p>This is your project’s dashboard. Use the navigation to access features.</p>
+
+</div>
